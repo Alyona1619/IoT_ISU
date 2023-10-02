67350e72b6ab964f4e3e0b1d83b56d602f6a0dc8
Alyona1619 <108504553+Alyona1619@users.noreply.github.com>
2023-10-02 23:26:16 +0800

    Create main.ino

---
+++ LedPhotoInteracive/main.ino
@@ -0,0 +1,105 @@
+#define sensor_pin A0
+#define led_pin 2
+
+bool streaming = false;
+bool send_one_value = false;
+long previous_send_time = 0;
+long send_count = 0;
+
+bool isAuto = false;
+bool isAlarm = false;
+
+int sensor_limit = 850;
+
+void setup() 
+{
+  Serial.begin(9600);
+}
+
+void loop() 
+{
+  long current_time = millis();
+  data_reading();
+  
+
+  if (streaming == true and current_time / 100 != send_count){
+    send_photo_data();
+    //previous_send_time = current_time;
+    previous_send_time = previous_send_time + 100;
+    send_count = current_time / 100;
+    Serial.print("Time in millis ");
+    Serial.print(millis());
+  }
+  if (send_one_value == true){
+    send_photo_data();
+    send_one_value = false;
+  }
+
+
+  if (isAuto){
+    int sensor_value = analogRead(sensor_pin);
+    if (sensor_value < sensor_limit) 
+    {
+      digitalWrite(led_pin, HIGH);
+    }
+    else 
+    {
+      digitalWrite(led_pin, LOW);
+    }
+  }
+
+
+  if (isAlarm) {
+    
+  }
+}
+
+void data_reading()
+{
+  if (Serial.available() > 0)
+    {
+        char command = Serial.read();
+        switch (command)
+        {
+        case 'p': // for Photo sensor
+            streaming = true;
+            break;
+        case 's': // single value
+            streaming = false;
+            send_one_value = true;
+            break;
+        case 'i': // illumination level (auto mode)
+            isAlarm = false;
+            isAuto = true;
+            break;
+        case 'n': // on (hand mode)
+            isAuto = false;
+            isAlarm = false;
+            digitalWrite(led_pin, HIGH);
+            break;
+        case 'f': // off (hand mode)
+            isAuto = false;
+            isAlarm = false;
+            digitalWrite(led_pin, LOW);
+            break;
+        case 'a': // alarm system
+            isAuto = false;
+            isAlarm = true;
+            break;
+        //case 'r': // reset
+        //    alarmMode = false;
+        //    lightControlMode = false;
+        //    shouldReadSensor = false;
+        //    digitalWrite(led_pin, LOW);
+        //    break;
+        
+        }
+    }
+}
+
+void send_photo_data()
+{
+  int val = analogRead(sensor_pin);
+  Serial.print("Sensor value: ");
+  Serial.println(val);
+}
