e3ca8c342c59dd5cd35f14b50a357055557721fa
persik <108504553+Alyona1619@users.noreply.github.com>
2024-02-15 23:55:53 +0800

    add pretty output and refactor

--- ButtonMatrix/buttonmatrix.ino
+++ ButtonMatrix/buttonmatrix.ino
@@ -1,59 +1,66 @@
-#define PIN_D8 8 
-#define PIN_D9 9 
-#define PIN_D10 10 
-#define PIN_D11 11 
+const int numRows = 2;
+const int numCols = 2;
+const int rowPins[numRows] = {8, 9};
+const int colPins[numCols] = {10, 11};
 
-char pushed[2][2] = {{0, 0}, {0, 0}}; // массив нажатых кнопок
-int k = 0; // счетчик нажатий
-const int rows[2] = {PIN_D8, PIN_D9};
-const int cols[2] = {PIN_D10, PIN_D11};
-bool currButtonState[2][2] = {{false, false}, {false, false}};
-bool prevButtonState[2][2] = {{false, false}, {false, false}};
+bool currButtonState[numRows][numCols];
+bool prevButtonState[numRows][numCols];
+char pushed[numRows][numCols];
 
-void setup()
-{
-  pinMode(PIN_D8, OUTPUT);
-  pinMode(PIN_D9, OUTPUT);
-  pinMode(PIN_D10, INPUT_PULLUP);
-  pinMode(PIN_D11, INPUT_PULLUP);
+void setup() {
 
-  digitalWrite(PIN_D8, HIGH);
-  digitalWrite(PIN_D9, HIGH);
+  for (int i = 0; i < numRows; i++) {
+    pinMode(rowPins[i], OUTPUT);
+    digitalWrite(rowPins[i], HIGH);
+  }
 
+  for (int j = 0; j < numCols; j++) {
+    pinMode(colPins[j], INPUT_PULLUP);
+  }
 
-  
+  for (int i = 0; i < numRows; i++) {
+    for (int j = 0; j < numCols; j++) {
+      currButtonState[i][j] = false;
+      prevButtonState[i][j] = false;
+      pushed[i][j] = '0'; 
+    }
+  }
+
   Serial.begin(9600);
 }
 
-void loop()
-{
+void loop() {
+
   bool stateChanged = false;
 
-  for (int i = 0; i < 2; i++) {
-    digitalWrite(rows[i], LOW);
-    
-    for (int j = 0; j < 2; j++) {
-      currButtonState[i][j] = digitalRead(cols[j]);
+  for (int i = 0; i < numRows; i++) {
+    digitalWrite(rowPins[i], LOW);
+    delay(30);
+    for (int j = 0; j < numCols; j++) {
+      currButtonState[i][j] = digitalRead(colPins[j]);
+
       if (currButtonState[i][j] != prevButtonState[i][j]) {
         stateChanged = true;
+
         if (!currButtonState[i][j]) {
-          pushed[i][j] = '0'; 
+          pushed[i][j] = '1'; 
         } else {
-          pushed[i][j] = '1';
+          pushed[i][j] = '0'; 
         }
       }
       prevButtonState[i][j] = currButtonState[i][j];
     }
-    digitalWrite(rows[i], HIGH);
+    digitalWrite(rowPins[i], HIGH);
   }
+
+  if (stateChanged && !(pushed[0][0] == '0' && pushed[0][1] == '0' && pushed[1][0] == '0' && pushed[1][1] == '0')) {
     
-    for (int i = 0; i < 2; i++) {
-      for (int j = 0; j < 2; j++) {
+    for (int i = 0; i < numRows; i++) {
+      for (int j = 0; j < numCols; j++) {
         Serial.print(pushed[i][j]);
       }
     }
-    Serial.println();
+    Serial.println(); 
   }
-  delay(100);
-}
+  delay(100); 
 }
